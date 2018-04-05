b183b478bc65b9802af9db348da04f2762723c69
Ryan Greenlee <Ryan@kimberlys-macbook-pro.local>
2018-04-05 12:15:42 -0400

    Initial Commit

---
+++ Favorite Things/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favorite Things
+//
+//  Created by Ryan Greenlee on 4/5/18.
+//  Copyright © 2018 Ryan Greenlee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
